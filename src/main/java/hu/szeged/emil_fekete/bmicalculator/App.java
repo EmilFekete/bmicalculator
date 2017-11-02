099cee819dbe205adb2761c74488aeb6d45b1802
Fekete Emil <fekete.feketeemil>
2017-11-02 13:19:45 +0100

    Init commit

---
+++ src/main/java/hu/szeged/emil_fekete/bmicalculator/App.java
@@ -0,0 +1,13 @@
+package hu.szeged.emil_fekete.bmicalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
